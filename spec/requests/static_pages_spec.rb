5f0e0a41373f57dec58d3d5403d55b54b1b9a722
michael <mike_smo@yahoo.com>
2013-02-16 21:48:27 +1100

    Add automated testing 'Gaurd/Spork' and  a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,14 +1,20 @@
 require 'spec_helper'
 
-describe "Static pages" do 
+describe "Static pages" do
 
   describe "Home page" do 
 
     it "should have the h1 'Sample App'" do 
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
-    end 
+    end
 
+    it "should have the base title"  do 
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+    
     it "should have the title 'Home'" do 
       visit '/static_pages/home'
       page.should have_selector('title',
